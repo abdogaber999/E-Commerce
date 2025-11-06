cf0fa120f61bfcf14f1fd0fef5d6856f025ea1e3
abdogaber999 <153696740+abdogaber999@users.noreply.github.com>
2025-11-06 08:28:31 +0200

    Add project files.

---
+++ E-Commerce.Web/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name Is Required")]
+        [MaxLength(3 , ErrorMessage = "Length Not Valid")]
+        public string Name { get; set; }
+    }
+}
